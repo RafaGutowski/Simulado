9b03ee4ccd42f9c63a80af531d47f9c956bbdbc4
RafaGutowski <162761689+RafaGutowski@users.noreply.github.com>
2024-06-24 09:22:32 -0300

    Pessoas e Enderecos .CSV

---
+++ SimuladoJava/src/br/edu/up/Pessoas.java
@@ -0,0 +1,46 @@
+package br.edu.up;
+
+public class Pessoas {
+
+    private int codigo;
+    private String nome;
+    private String rua;
+    private String cidade;
+    
+    
+    public Pessoas(int codigo, String nome, String rua, String cidade) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.rua = rua;
+        this.cidade = cidade;
+    }
+    public int getCodigo() {
+        return codigo;
+    }
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getRua() {
+        return rua;
+    }
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+    public String getCidade() {
+        return cidade;
+    }
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+   
+    public String toCSV() {
+        return null;
+    }
+    
+}
